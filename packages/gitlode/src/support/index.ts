00702ed5bf61a43a8358c53257de05b52bfff37e
Tomokazu Wakabayashi <187154953+tomo-waka@users.noreply.github.com>
2026-07-01 12:47:45 +0900

    feat(queue): implement PriorityQueue and OrderedQueue with common behavior tests

--- packages/gitlode/src/support/index.ts
+++ packages/gitlode/src/support/index.ts
@@ -9,4 +9,12 @@ export {
 } from "./helpers.js";
 export { formatUnixTimestampWithOffset } from "./date.js";
 export type { AbsolutePath, AbsoluteDirectoryPath, IsoDateTimeString } from "./type.js";
+export type {
+  WorkQueue,
+  Compare,
+  DequeueOrder,
+  BlockOrder,
+  OrderedQueueOptions,
+} from "./work-queue.js";
+export { PriorityQueue, OrderedQueue } from "./work-queue.js";
 export { dirnameOfFilePath, resolveFilePath } from "./path.js";
